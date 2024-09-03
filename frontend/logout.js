a9b5290ffb57453f38d736277e60b18184dac29b
sr-roboto <marcoslopez123.lu@gmail.com>
2024-09-03 15:47:16 -0300

    Actualizar estilos de las páginas frontend y implementar la pagina register para agregar un usuario nuevo a la base de datos

---
+++ frontend/logout.js
@@ -0,0 +1,26 @@
+// Manejar el cierre de sesión
+document.getElementById('logout').addEventListener('click', async () => {
+  const response = await fetch('http://localhost:4000/api/auth/logout', {
+    method: 'POST',
+    credentials: 'include',
+  });
+
+  if (!response.ok) {
+    throw new Error('Error al cerrar sesión');
+  } else {
+    window.location.href = 'index.html';
+  }
+});
+
+const logout = async () => {
+  const response = await fetch('http://localhost:4000/api/auth/logout', {
+    method: 'POST',
+    credentials: 'include',
+  });
+
+  if (!response.ok) {
+    throw new Error('Error al cerrar sesión');
+  } else {
+    window.location.href = 'index.html';
+  }
+};
