4606d15a6063db66dfbb89590258fd440a80db2b
sr-roboto <marcoslopez123.lu@gmail.com>
2024-09-04 11:53:39 -0300

    Agregar la funcionalidad de logout y creación de la página register

--- frontend/main.js
+++ frontend/main.js
@@ -14,17 +14,3 @@
     window.location.href = 'index.html';
   }
 })();
-
-// Manejar el cierre de sesión
-document.getElementById('logout').addEventListener('click', async () => {
-  const response = await fetch('http://localhost:4000/api/auth/logout', {
-    method: 'POST',
-    credentials: 'include',
-  });
-
-  if (!response.ok) {
-    throw new Error('Error al cerrar sesión');
-  } else {
-    window.location.href = 'index.html';
-  }
-});
